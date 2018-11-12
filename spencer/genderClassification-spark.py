4ef0c19789171f265581b11a5b4bc179e524a4d0
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2018-11-11 17:28:59 -0800

    modifying notebook for use with spark

---
+++ spencer/genderClassification-spark.py
@@ -0,0 +1,18 @@
+# use findspark to find all the newly installed modules
+import findspark
+findspark.init()
+
+# Numerical Imporst 
+import pandas as pd
+import numpy as np
+import scipy 
+
+# Plotting 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Python 
+import os
+
+# scipy
+from scipy.cluster import hierarchy as hc # for dendograms
