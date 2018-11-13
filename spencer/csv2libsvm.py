c8a7c7969b641607f9c3913364c4c1cc69b9b731
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2018-11-13 15:00:50 -0800

    converted sum_tab_1 to libsvm

--- spencer/csv2libsvm.py
+++ spencer/csv2libsvm.py
@@ -10,21 +10,21 @@ Expecting no headers. If present, headers can be skipped with argv[4] == 1.
 import sys
 import csv
 from collections import defaultdict
-
+ 
 def construct_line( label, line ):
-	new_line = []
-	if float( label ) == 0.0:
-		label = "0"
-	new_line.append( label )
+    new_line = []
+    if float( label ) == 0.0:
+        label = "0"
+    new_line.append( label )
 
-	for i, item in enumerate( line ):
-		if item == '' or float( item ) == 0.0:
-			continue
-		new_item = "%s:%s" % ( i + 1, item )
-		new_line.append( new_item )
-	new_line = " ".join( new_line )
-	new_line += "\n"
-	return new_line
+    for i, item in enumerate( line ):
+        if item == '' or float( item ) == 0.0:
+            continue
+        new_item = "%s:%s" % ( i + 1, item )
+        new_line.append( new_item )
+    new_line = " ".join( new_line )
+    new_line += "\n"
+    return new_line
 
 # ---
 
